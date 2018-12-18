b4150934244caefacaca4b0d1ea8dbb7ab352b39
Brian Adamson <brianadamson87@gmail.com>
2018-12-18 13:16:04 -0700

    comment routes

--- server/server-assets/routes/comment.js
+++ server/server-assets/routes/comment.js
@@ -4,14 +4,51 @@ let Comment = require('../models/comment')
 
 
 // Create a comment
-
+router.post('/:postId', (req, res, next) => {
+  req.body.creatorId = req.session.uid
+  Comment.create(req.body)
+    .then(newComment => {
+      res.send(newComment)
+      console.log(newComment)
+    })
+    .catch(err => {
+      console.log(err)
+      next()
+    })
+})
 
 
 // Delete a comment by Id
 
+router.delete('/:id', (req, res, next) => {
+  Comment.findById(req.params.id)
+    .then(comment => {
+      if (!comment.creatorId.equals(req.session.uid)) {
+        return res.status(401).send("Access Denied!")
+      }
+      comment.remove(err => {
+        if (err) {
+          console.log(err)
+          next()
+          return
+        }
+        res.send("Successfully Deleted Post")
+      })
+    })
+})
+
 
 // get all comments by post Id
 
-
+router.get('/:postId', (req, res, next) => {
+  Comment.find({})
+    .then(data => {
+      res.send(data)
+    })
+    .catch(err => {
+      console.log(err)
+      next()
+    })
+})
 
 module.exports = router
