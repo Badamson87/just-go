a9da338483fe4a83c286c7390dda0e8c0c6f6d46
Aubrey Stento <aubreystento@gmail.com>
2018-12-20 10:35:35 -0700

    user dash button

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,18 +2,28 @@
   <div id="app">
     <div id="nav">
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#/">Just Go Home</a>
+        <a class="navbar-brand" href="#/">Just Go (logo)</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="#/userDash" v-if="user._id">UserDash</a>
+                  <a class="nav-link" href="#/auth" @click="logout" v-if="!user._id"></a>
+                </li>
+              </ul>
           <ul class="navbar-nav mr-auto">
             <li class="nav-item">
               <a class="nav-link" href="#/auth" v-if="!user._id">Login/Register</a>
               <a class="nav-link" href="#/auth" @click="logout" v-if="user._id">Logout</a>
             </li>
           </ul>
+
+
           <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
