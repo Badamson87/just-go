3d851e686cfa44faf492c66ce547b139ff358fe0
Kimberly Streicher <kimberlystreicher@gmail.com>
2019-01-02 13:46:31 -0700

    hide delete button

--- client/src/views/Album.vue
+++ client/src/views/Album.vue
@@ -13,7 +13,7 @@
           <p>{{post.title}}</p>
           <p>{{post.location}}</p>
           <p>{{post.description}}</p>
-          <button class="btn btn-info" @click="deletePost(post)">Delete</button>
+          <button v-if="activeAlbum[0].creatorId == user._id" class="btn btn-info" @click="deletePost(post)">Delete</button>
           <!-- add v-if here to be able to delete if they are te user -->
         </div>
       </div>
