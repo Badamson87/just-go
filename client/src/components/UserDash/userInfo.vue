336069c015e6d78d3d3bbf7724d007cf77c8965b
carissa morrow <carissamorrow@hotmail.com>
2019-01-03 11:12:18 -0700

    can upload image but its large

--- client/src/components/UserDash/userInfo.vue
+++ client/src/components/UserDash/userInfo.vue
@@ -1,24 +1,36 @@
 <template>
-  <div class="userInfo card cardBorder">
-    <p>User Info</p>
-    <div class="imgCard">
-      <img class="userImg" :src="user.image" />
-    </div>
-    <h5 class="mt-4">{{user.name}}</h5>
-    <input type="file" @change="onFileChanged">
-    <button @click="onUpload">Upload!</button>
+  <div>
+    <div class="">
+      <h2>Upload file</h2>
+      <div>
+        <vue-base64-file-upload class="v1" accept="image/png,image/jpeg" image-class="v1-image" input-class="v1-image"
+          :max-size="customImageMaxSize" @size-exceeded="onSizeExceeded" @file="onFile" @load="onLoad" />
+      </div>
 
+    </div>
+    <div class="userInfo card cardBorder">
+      <p>User Info</p>
+      <div class="imgCard">
+        <img class="userImg" :src="user.image" />
+      </div>
+      <h5 class="mt-4">{{user.name}}</h5>
+      <!-- <input type="file" @change="onFileChanged">
+        <button @click="onUpload">Upload!</button> -->
+    </div>
   </div>
 </template>
 
 <!-- need user info get requests -->
 
 <script>
+  import VueBase64FileUpload from 'vue-base64-file-upload';
+
+
   export default {
     name: 'UserInfo',
     data() {
       return {
-        selectedFile: null
+        customImageMaxSize: 3
       }
     },
     computed: {
@@ -30,10 +42,24 @@
       onFileChanged(event) {
         this.selectedFile = event.target.files[0]
       },
-      onUpload() {
-        axios.post('my-domain.com/file-upload', this.selectedFile)
+      onFile(file) {
+        console.log(file); // file object
+      },
+
+      onLoad(dataUri) {
+        dispatch(dataUri); // data-uri string
+      },
+
+      onSizeExceeded(size) {
+        alert(`Image ${size}Mb size exceeds limits of ${this.customImageMaxSize}Mb!`);
       }
-    }
+      // onUpload() {
+      //   axios.post('my-domain.com/file-upload', this.selectedFile)
+      // }
+    },
+    components: {
+      VueBase64FileUpload
+    },
   }
 
 </script>
@@ -46,5 +72,9 @@
   .userImg {
     height: 20vh;
     width: 20vh;
+  }
+
+  .v1 {
+    size: 200px
   }
 </style>
