637b967504a7defcd31688beca21893eafc469e4
carissa morrow <carissamorrow@hotmail.com>
2019-01-11 17:49:41 -0700

    working,....

--- client/src/components/UserDash/userInfo.vue
+++ client/src/components/UserDash/userInfo.vue
@@ -62,17 +62,17 @@
   }
 
   .userInfo {
-    max-width: 90%;
+    max-width: 60%;
     max-height: 40px;
   }
 
   .v1-image {
     object-fit: fill;
-    max-width: 90%;
+    max-width: 60%;
     max-height: 40%;
   }
 
   .position {
-    max-width: 90%
+    max-width: 60%
   }
 </style>
