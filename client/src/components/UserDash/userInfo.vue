79701d805baa40ea21e55a0fec09328261578bd0
Brian Adamson <brianadamson87@gmail.com>
2019-01-11 19:38:33 -0700

    picture

--- client/src/components/UserDash/userInfo.vue
+++ client/src/components/UserDash/userInfo.vue
@@ -4,12 +4,12 @@
       <h5>{{user.name}}</h5>
     </div>
     <!-- <button class="btn btn-info" @click="addBio">Add Bio</button> -->
+    <div class="userInfo">
+      <img class="v1-image" :src="user.image" alt="">
+    </div>
     <div>
       <vue-base64-file-upload :disable-preview="true" class="v1-image" accept="image/png,image/jpeg" image-class="v1-image"
         input-class="v1-image" :max-size="customImageMaxSize" @size-exceeded="onSizeExceeded" @file="onFile" @load="onLoad" />
-    </div>
-    <div class="userInfo">
-      <img class="v1-image" :src="user.image" alt="">
     </div>
 
   </div>
